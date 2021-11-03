9af010a235b4c03d7b8bcbe99e70e42217ff35e1
SMWeed19 <93607502+SMWeed19@users.noreply.github.com>
2021-11-03 16:53:52 -0400

    Add files via upload

--- Problem_1.py
+++ Problem_1.py
@@ -4,3 +4,11 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+def find_multiples(m, n):
+    multiples_of_m_below_n = []
+    for i in range(1, n):
+        if i % m == 0:
+            multiples_of_m_below_n.append(i)
+    return multiples_of_m_below_n
+
+print(find_multiples(3, 10))
